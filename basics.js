ff3c86d9aaab48c4d0651e1388a1bd2b80d91b71
paljain08 <pallavijain@benchmarkit.solutions>
2021-08-20 02:16:44 +0530

    data types

--- basics.js
+++ basics.js
@@ -16,3 +16,33 @@ let $function=27;
 let pallavi_jain='pj';
 console.log($function);
 console.log(pallavi_jain);
+
+//Data types
+
+//1. Boolean
+let nothingIsImpossible=true;
+console.log(nothingIsImpossible);
+
+console.log(typeof true);
+
+//2.String
+ //chnage the value boolean type to string type
+
+ nothingIsImpossible="Yes"
+ console.log(typeof nothingIsImpossible);
+
+ //3. Undefined
+ let name;
+ console.log(name);
+ console.log(typeof name);
+
+ //dynamically change their values
+
+ name="Pallavi";
+ console.log(typeof name);
+
+ //4.Number
+ let num=20;
+ console.log(num);
+
+ 
