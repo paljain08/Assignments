54d48412b49e21e4b1287e8c837604223132b779
paljain08 <pallavijain@benchmarkit.solutions>
2021-08-20 01:27:48 +0530

    name conventions

--- basics.js
+++ basics.js
@@ -9,3 +9,10 @@ let mySecondAddress="Pune";
 
 console.log(myFirstAddress);
 console.log(mySecondAddress);
+
+//variable name conventions
+
+let $function=27;
+let pallavi_jain='pj';
+console.log($function);
+console.log(pallavi_jain);
